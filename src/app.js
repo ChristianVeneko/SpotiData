cf00456ef5b9c9a10d1a87917f420c44980daf21
Christian Mendoza <christianmendozater@gmail.com>
2024-04-19 22:47:59 -0400

    original app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
+// app.js
+import path from 'path';
 import express from 'express'
+import session from 'express-session';
 import morgan from 'morgan'
-import { fileURLToPath } from 'url'
 
+import { Login, callback } from './routes/auth.routes.js'
 const app = express()
+
+app.use(session({
+    secret: 'yolavienuntaxi',
+    resave: false,
+    saveUninitialized: true
+  }));
+
+app.use(express.static(path.resolve('./src/public/')));
+const htmlPath  = path.resolve('./src/public/html');
+
 app.use(morgan('dev'))
 app.get('/', (req, res) => {
-    res.sendFile('public/index.html', { root: fileURLToPath(import.meta.url).replace('app.js', '') })
+
+  if(!req.session.token) {
+    return res.sendFile('login.html', { root: htmlPath});
+  }
+  return res.sendFile('index.html', { root: htmlPath});
+
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 })
+
+app.get('/api/session', (req, res) => {
+    res.json(req.session);
+  });
+
+app.get('/login', Login)
+
+app.get('/callback', callback)
+
 
 export default app

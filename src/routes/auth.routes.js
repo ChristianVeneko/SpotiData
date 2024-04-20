4e822181486187e84ed96ce9319b6464fc0eb2d7
Christian Mendoza <christianmendozater@gmail.com>
2024-04-20 00:46:25 -0400

    callback changes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -12,7 +12,7 @@ export const callback = (req, res) => {
 
   if (error) {
     console.error("Callback Error:", error);
-    res.send(`Callback Error: ${error}`);
+    res.redirect("/login");
     return;
   }
 
@@ -21,32 +21,15 @@ export const callback = (req, res) => {
     .then((data) => {
       const access_token = data.body["access_token"];
       const refresh_token = data.body["refresh_token"];
-      const expires_in = data.body["expires_in"];
 
-      spotifyApi.setAccessToken(access_token);
-      spotifyApi.setRefreshToken(refresh_token);
-
+      req.session.access_token = access_token; 
       console.log("access_token:", access_token);
       console.log("refresh_token:", refresh_token);
 
-      req.session.token = access_token;
-
-      console.log(
-        `Sucessfully retreived access token. Expires in ${expires_in} s.`
-      );
-      res.redirect("/");
-
-      setInterval(async () => {
-        const data = await spotifyApi.refreshAccessToken();
-        const access_token = data.body["access_token"];
-
-        console.log("The access token has been refreshed!");
-        console.log("access_token:", access_token);
-        spotifyApi.setAccessToken(access_token);
-      }, (expires_in / 2) * 1000);
+      res.redirect("/frontend"); 
     })
     .catch((error) => {
       console.error("Error getting Tokens:", error);
-      res.send(`Error getting Tokens: ${error}`);
+      res.redirect("/login"); 
     });
 };
